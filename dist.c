0a353bfdd548e0779546ba32277801917d3e592c
James Potter <jrp50@bath.ac.uk>
2018-12-24 19:04:02 +0000

    Implemented settling of values

--- dist.c
+++ dist.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <mpi.h>
 
 MPI_Comm MPI_COMM_COMPUTE;
@@ -31,22 +32,23 @@ void printInfo(double* workingArr, int size, int dim, int rank) {
         if (i % dim == 0) strcat(strArr, "\n");
         strcat(strArr, s);
     }
+    free(s);
     printf("Processor with rank %d has a working array of: \n%s\n", rank, strArr);
 }
 
-double* initMasterProc(double* arr, int dim, double precision, int *size) {
+double* initMasterProc(double* arr, int* elemsToCompute, int* indices, 
+        int dim, double precision, int *size) {
     int numProcs;
     MPI_Comm_size(MPI_COMM_COMPUTE, &numProcs);
 
     double *workingArr;
 
     int* rows = rowsPerProc(numProcs, dim);
-    int elemsToCompute[numProcs];
     for (int i = 0; i < numProcs; i++) {
         elemsToCompute[i] = rows[i] * dim;
     }
+    free(rows);
 
-    int indices[numProcs];
     int nextIndex = 0;
     for (int i = 0; i < numProcs; i++) {
         indices[i] = nextIndex;
@@ -54,7 +56,6 @@ double* initMasterProc(double* arr, int dim, double precision, int *size) {
         nextIndex = elemsToCompute[i] + prevIndex;
     }
 
-    rows = &rows[0];
     *size = elemsToCompute[0] + (2*dim);
     workingArr = malloc(sizeof(double) * *size);
     memcpy(workingArr, &arr[0], sizeof(double) * *size);
@@ -69,7 +70,6 @@ double* initMasterProc(double* arr, int dim, double precision, int *size) {
                 MPI_COMM_COMPUTE, &req[i-1]);
     }
     MPI_Waitall(numProcs-1, req, statuses);
-
     return workingArr;
 }
 
@@ -79,7 +79,8 @@ double* initSlaveProc(int dim, double precision, int size) {
     MPI_Comm_size(MPI_COMM_COMPUTE, &numProcs);
 
     workingArr = malloc(sizeof(double) * size);
-    MPI_Recv(workingArr, size, MPI_DOUBLE, 0, 0, MPI_COMM_COMPUTE, MPI_STATUS_IGNORE);
+    MPI_Recv(workingArr, size, MPI_DOUBLE, 0, 0, 
+            MPI_COMM_COMPUTE, MPI_STATUS_IGNORE);
 
     return workingArr;
 }
@@ -92,7 +93,12 @@ double avgElem(double* arr, int index, int dim) {
     return (up+down+left+right)/4;
 }
 
-void averageRow(double* workingArr, double* avgArr, int dim, int row) {
+int fequal(double a, double b, double precision) {
+    return fabs(a-b) < precision;
+}
+
+void averageRow(double* workingArr, double* avgArr, int dim, int row,
+         int *settled, double precision) {
     int startIndex = row * dim;
 
     // fixed elements at edge of matrix
@@ -101,16 +107,18 @@ void averageRow(double* workingArr, double* avgArr, int dim, int row) {
 
     // average other elements in row
     for (int i = 1; i < dim-1; i++) {
-        avgArr[startIndex+i] = avgElem(workingArr, startIndex+i, dim); 
+        double avg = avgElem(workingArr, startIndex+i, dim);
+
+        // update settled flag if within precision
+        if (*settled == 1 && !fequal(avg, workingArr[startIndex+i], precision)) {
+            *settled = 0;
+        }
+
+        avgArr[startIndex+i] = avg;
     }
 }
 
-double* relax(double* workingArr, int size, int dim, double precision) {
-    int numProcs;
-    int rank;
-    MPI_Comm_size(MPI_COMM_COMPUTE, &numProcs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+void relax(double* workingArr, int size, int dim, double precision, int numProcs, int rank) {
     int numRows = size/dim;
     int *rowIndices = malloc(sizeof(int) * numRows);
     double* avgArr = malloc(sizeof(double) * size);
@@ -120,83 +128,152 @@ double* relax(double* workingArr, int size, int dim, double precision) {
         rowIndices[i] = nextRowIndex;
         nextRowIndex += dim;
     }
-    
+
     int topEdge = rank == 0;
     int bottomEdge = rank == numProcs-1;
 
-    MPI_Request topRowReq, bottomRowReq;
-    MPI_Request topRowSend, bottomRowSend;
-    // async receive top and bottom rows
-    averageRow(workingArr, avgArr, dim, 1);
-    if (topEdge) {
-        memcpy(&avgArr[0], &workingArr[0], sizeof(double) * dim);
-    } else {
-        MPI_Isend(&avgArr[dim], dim, MPI_DOUBLE, rank-1, 3, MPI_COMM_COMPUTE, &topRowSend);
-        MPI_Irecv(&avgArr[0], dim, MPI_DOUBLE, rank-1, 4, MPI_COMM_COMPUTE, &topRowReq);
-    }
+    MPI_Request topRowReq, topRowSend;
+    MPI_Request bottomRowReq, bottomRowSend;
+    MPI_Request settledReq[numProcs], settledSend[numProcs];
 
-    averageRow(workingArr, avgArr, dim, numRows-2);
-    int lastI = rowIndices[numRows-1];
-    int secondLastI = rowIndices[numRows-2];
-    if (bottomEdge) {
-        memcpy(&avgArr[lastI], &workingArr[lastI], sizeof(double) * dim);
-    } else {
-        MPI_Isend(&avgArr[secondLastI], dim, MPI_DOUBLE, rank+1, 4, MPI_COMM_COMPUTE, &bottomRowSend);
-        MPI_Irecv(&avgArr[lastI], dim, MPI_DOUBLE, rank+1, 3, MPI_COMM_COMPUTE, &bottomRowReq);
-    }
+    int procSettled[numProcs];
+    int settled = 0;
 
-    for (int i = 2; i < numRows-2; i++) {
-        averageRow(workingArr, avgArr, dim, i);            
-    }
+    // start loop here
+    while (!settled) {
 
-    if (!topEdge) {
-        MPI_Wait(&topRowReq, MPI_STATUS_IGNORE);
-        MPI_Wait(&topRowSend, MPI_STATUS_IGNORE);
-    }
-    
-    if (!bottomEdge) {
-        MPI_Wait(&bottomRowReq, MPI_STATUS_IGNORE);
-        MPI_Wait(&bottomRowSend, MPI_STATUS_IGNORE);
-    }
+        // reset settled
+        settled = 1;
 
-    return avgArr;
+        averageRow(workingArr, avgArr, dim, 1, &settled, precision);
+        if (topEdge) {
+            memcpy(&avgArr[0], &workingArr[0], sizeof(double) * dim);
+        } else {
+            MPI_Isend(&avgArr[dim], dim, MPI_DOUBLE, rank-1, 3,
+                    MPI_COMM_COMPUTE, &topRowSend);
+            MPI_Irecv(&avgArr[0], dim, MPI_DOUBLE, rank-1, 4,
+                    MPI_COMM_COMPUTE, &topRowReq);
+        }
+
+        averageRow(workingArr, avgArr, dim, numRows-2, &settled, precision);
+        int lastI = rowIndices[numRows-1];
+        int secondLastI = rowIndices[numRows-2];
+        if (bottomEdge) {
+            memcpy(&avgArr[lastI], &workingArr[lastI], sizeof(double) * dim);
+        } else {
+            MPI_Isend(&avgArr[secondLastI], dim, MPI_DOUBLE, rank+1, 4, 
+                    MPI_COMM_COMPUTE, &bottomRowSend);
+            MPI_Irecv(&avgArr[lastI], dim, MPI_DOUBLE, rank+1, 3, 
+                    MPI_COMM_COMPUTE, &bottomRowReq);
+        }
+
+        // bulk of processing here, averages all the other rows
+        for (int i = 2; i < numRows-2; i++) {
+            averageRow(workingArr, avgArr, dim, i, &settled, precision);            
+        }
+
+        // send settled value to other processes
+        procSettled[rank] = settled;
+        for (int i = 0; i < numProcs; i++) {
+            if (i != rank) {
+                MPI_Isend(&settled, 1, MPI_INT, i, 2, 
+                        MPI_COMM_COMPUTE, &settledSend[i]);
+                MPI_Irecv(&procSettled[i], 1, MPI_INT, i, 2, 
+                        MPI_COMM_COMPUTE, &settledReq[i]); 
+            }
+        }
+
+        if (!topEdge) {
+            MPI_Wait(&topRowReq, MPI_STATUS_IGNORE);
+            MPI_Wait(&topRowSend, MPI_STATUS_IGNORE);
+        }
+
+        if (!bottomEdge) {
+            MPI_Wait(&bottomRowReq, MPI_STATUS_IGNORE);
+            MPI_Wait(&bottomRowSend, MPI_STATUS_IGNORE);
+        }
+
+        // settled send and recv waits
+        for (int i = 0; i < numProcs; i++) {
+            if (i != rank) {
+                MPI_Wait(&settledSend[i], MPI_STATUS_IGNORE);
+                MPI_Wait(&settledReq[i], MPI_STATUS_IGNORE);
+            }
+        }
+
+        for (int i = 0; i < numProcs; i++) {
+            if (procSettled[i] == 0) {
+                settled = 0;
+                break;
+            }
+        }
+
+        // swap avgArr and workingArr pointers if not settled
+        if (!settled) {
+            double* temp = avgArr;
+            avgArr = workingArr;
+            workingArr = temp;
+        }
+    }  
+    free(rowIndices);
+    free(avgArr);
 }
 
-// TAG 0 is the array
+// TAG 0 is the initial array
 // TAG 1 is the size
 // TAG 2 is the settle flag
-// TAG 3 is updated rows
+// TAG 3 is updated rows (top)
+// TAG 4 is updated rows (bottom)
 int main() {
-    int dim = 5;
-    int precision = 0;
+    int dim = 6;
+    double precision = 0.01;
 
     MPI_Init(NULL, NULL);
     int rank;    
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     int inUse = rank < dim-2;
     MPI_Comm_split(MPI_COMM_WORLD, inUse, rank, &MPI_COMM_COMPUTE);
+
+    int numProcs;
+    MPI_Comm_size(MPI_COMM_COMPUTE, &numProcs);
     if (!inUse) {
         MPI_Finalize();
         exit(0);
     }
-
+    
+    int *elemsToCompute, *indices;
     double *workingArr;
     int size;
     if (rank == 0) {
+        elemsToCompute = malloc(sizeof(int) * numProcs);
+        indices = malloc(sizeof(int) * numProcs);
+        
         // read array from file
-        //double arr[36] = {1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,1,1,1,1,1};
-        double arr[25] = {1,1,1,1,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,1,1,1,1};
-        workingArr = initMasterProc(&arr[0], dim, precision, &size);
+        double arr[36] = {1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,1,1,1,1,1};
+        workingArr = initMasterProc(&arr[0], elemsToCompute, indices, dim, precision, &size);
     } else {
         MPI_Recv(&size, 1, MPI_INT, 0, 1, MPI_COMM_COMPUTE, MPI_STATUS_IGNORE);
         workingArr = initSlaveProc(dim, precision, size);
     }
 
-    double *newArr = relax(workingArr, size, dim, precision);
-    printInfo(newArr, size, dim, rank);
+    relax(workingArr, size, dim, precision, numProcs, rank);
+    printInfo(workingArr, size, dim, rank);
 
-    // relax working array, sending top and bottom rows to top and bottom ranks
-    // send out settled flag to everyone async
+    double *finalArr; 
+    if (rank == 0) {
+        finalArr = malloc(sizeof(double) * dim * dim);
+    }
+    
+    int computeSize = size - (dim*2);
+    MPI_Gatherv(&workingArr[dim], computeSize, MPI_DOUBLE, &finalArr[dim],
+            elemsToCompute, indices, MPI_DOUBLE, 0, MPI_COMM_COMPUTE);
+    
+    free(workingArr);
+    if (rank == 0) {
+        printInfo(finalArr, dim*dim, dim, rank);
+        free(finalArr);
+    }
 
     MPI_Finalize();
 }
